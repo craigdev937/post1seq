91a89e5e9076190c59ef0b14047d8c0280f482b2
Craig <indigomx9@gmail.com>
2022-06-02 01:17:23 -0400

    Created all CRUD Methods.

--- src/controllers/friendCon.js
+++ src/controllers/friendCon.js
@@ -26,7 +26,48 @@ class FriendsClass {
             res.status(500).json(res.statusText);
             next(error);
         }
-    };    
+    };
+
+    GetOne = async (req, res, next) => {
+        const { id } = req.params;
+        try {
+            const friend = await Friends.findOne({
+                where: { id }
+            });
+            return res.status(200).json(friend);
+        } catch (error) {
+            res.status(500).json(res.statusText);
+            next(error);
+        }
+    };
+
+    Update = async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const { first, last, age, info } = req.body;
+            const friend = await Friends.findByPk(id);
+                friend.first = first;
+                friend.last = last;
+                friend.age = age;
+                friend.info = info;
+            await friend.save();
+            return res.status(200).json(friend);
+        } catch (error) {
+            res.status(500).json(res.statusText);
+            next(error);
+        }
+    };
+
+    Delete = async (req, res, next) => {
+        const { id } = req.params;
+        try {
+            await Friends.destroy({where: { id }});
+            return res.sendStatus(204);
+        } catch (error) {
+            res.status(500).json("The Friend was deleted!");
+            next(error);
+        }
+    };
 };
 
 export const FRIENDS = new FriendsClass();
--- src/routes/friendRt.js
+++ src/routes/friendRt.js
@@ -4,5 +4,9 @@ import { FRIENDS } from "../controllers/friendCon.js";
 export const friendRt = express.Router();
     friendRt.post("/", FRIENDS.Create);
     friendRt.get("/", FRIENDS.FetchAll);
+    friendRt.get("/:id", FRIENDS.GetOne);
+    friendRt.put("/:id", FRIENDS.Update);
+    friendRt.delete("/:id", FRIENDS.Delete);
+    
 
 
